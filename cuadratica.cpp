ab3978c9bfdfa6282be8097dc982ae69cf1a1485
juliannGarcia <gamj080214hgtrrla0@cbtis217.edu.mx>
2024-10-16 07:50:56 -0600

    Modificado: eliminado comentarios y agregado un comentario general

--- cuadratica.cpp
+++ cuadratica.cpp
@@ -1,16 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main (){
-    ///aqui declare las variables
+    ///Este programa calcula las soluciones de una ecuación cuadrática
+
     int a,b,c;
     int x1,x2;
     int resultado;
-    ///aqui lei las variables
-    cout<<"Codigo creado por julian";
     cin>>a;
     cin>>b;
     cin>>c;
-    ///aqui hice las operaciones
     resultado=(b*b)-(4*a*c);
     x1=(-b+(resultado))/(2*a);
     x2=(-b-(resultado))/(2*a);
